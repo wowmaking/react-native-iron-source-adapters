54c2297e19bca4755fea20f29342c4ea339dde0a
Dmitriy Kazlouski <d.kazlouski@wowmaking.net>
2020-05-07 17:40:33 +0300

    Add RN module

--- packages/rnis-adcolony/android/src/main/java/net/wowmaking/ironsource/adcolony/RNIronSourceAdcolonyModule.java
+++ packages/rnis-adcolony/android/src/main/java/net/wowmaking/ironsource/adcolony/RNIronSourceAdcolonyModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package net.wowmaking.ironsource.adcolony;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- packages/rnis-adcolony/android/src/main/java/net/wowmaking/ironsource/adcolony/RNIronSourceAdcolonyPackage.java
+++ packages/rnis-adcolony/android/src/main/java/net/wowmaking/ironsource/adcolony/RNIronSourceAdcolonyPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package net.wowmaking.ironsource.adcolony;
 
 import java.util.Arrays;
 import java.util.Collections;
